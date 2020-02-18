779bf9cb35abbefb870aa43cea2b29cf305fe825
Michal Kijowski <contact@michalkijowski.pl>
2020-02-18 11:24:12 +0100

    Add service-worker registration

--- src/index.ts
+++ src/index.ts
@@ -63,4 +63,12 @@ document.onkeyup = () => {
   }
 }
 
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .catch(() => { console.error('Failed to load service worker') })
+  })
+}
+
 m.mount(document.body, App)
